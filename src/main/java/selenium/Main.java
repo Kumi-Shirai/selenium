094c9752784740fcf370ebf11d2eef679590fec8
Kumi Shirai <kumi.shirai@rakus-partners.co.jp>
2022-11-15 10:15:45 +0900

    最初のコミット

---
+++ src/main/java/selenium/Main.java
@@ -0,0 +1,33 @@
+package selenium;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Main {
+	
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.google.co.jp/");
+		WebElement element = driver.findElement(By.cssSelector("div.a4bIc > input"));
+		Thread.sleep(500);
+		element.sendKeys("CSSセレクタとは");
+		Thread.sleep(500);
+		element.sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		driver.close();
+		
+//		複数の要素（テーブルなど）を受け取る。
+//		List<WebElement> list = driver.findElements(By.cssSelector("div.a4bIc"));
+		
+	}
+
+}
